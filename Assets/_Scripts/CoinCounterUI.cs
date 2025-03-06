b2a110b0a7232162c1f85937f8e54eec9e58401d
Antonio Vazquez-Mackay <73569624+Tonydvm@users.noreply.github.com>
2025-03-06 09:03:33 -0800

    added coin counter with animation

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+    [SerializeField] private Ease animationCurve;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}");
+        // trigger the local move animation
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        // this tells the editor to wait for a given period of time
+        yield return new WaitForSeconds(duration);
+        // we use duration since that's the same time as the animation
+        current.SetText($"{score}"); // update the original score
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x,
+            containerInitPosition, localPosition.z);
+        // then reset the y-localPosition of the coinTextContainer
+    }
+}
